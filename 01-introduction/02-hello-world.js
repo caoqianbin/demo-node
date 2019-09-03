b394661dff6da616559c40024c95c6596a44554d
caoqian <1669125059@qq.com>
2019-09-03 18:12:48 +0800

    add 01-introduction demo code

--- 01-introduction/02-hello-world.js
+++ 01-introduction/02-hello-world.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const http = require('http');
-const hostname = '192.168.112.1';
-const port = 808;
+const hostname = '192.168.112.144';
+const port = 8080;
 
 const server = http.createServer((req,res)=>{
   res.statusCode = 200;
