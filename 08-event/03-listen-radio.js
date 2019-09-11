3ac433628f67d5cacdc709b953dbdfc26dfbf0b1
caoqian <1669125059@qq.com>
2019-09-11 16:27:52 +0800

    a

---
+++ 08-event/03-listen-radio.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Radio = require('./03-radio'),
+      log = console.log;
+
+
+const station = {
+  freq: '106.7',
+  name: 'music Radio'
+};
+
+var radio = new Radio(station);
+ 
+radio.on('play',(station) => {
+  log('"%s" FM %s opened!',station.name,station.freq);
+  log('lalala...');
+});
+radio.on('stop',(station) =>{
+  log('"%s" FM %s closed!',station.name,station.freq);
+});
+
+
+
+
+
