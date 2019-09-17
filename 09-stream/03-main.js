1e545cf9e5ed071b23a0d9c5904b7c557e99ecbb
caoqian <1669125059@qq.com>
2019-09-17 17:12:18 +0800

    add 09-stream code

---
+++ 09-stream/03-main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const MyReadable = require('./03-my-readable');
+
+var r= new MyReadable();
+
+r.pipe(process.stdout);
+
+
+
+
+
+
---
+++ 09-stream/04-main.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const GreenStream = require('./04-green-stream'),
+      stdin =process.stdin;
+
+var g = new GreenStream();
+
+stdin.resume();
+stdin.pipe(g);
+ 
+
+
+
+
+
+
